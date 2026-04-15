13c9e0351efb8badf752aa77587ecf9dfcbbc096
Dario Wenger <dariowenger@macbook-pro-von-dario.home>
2026-04-15 15:20:29 +0200

    tab bar working but not animated yet

--- myApp/App.tsx
+++ myApp/App.tsx
@@ -1,8 +1,16 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabs from './src/navigation/BottomTabs';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    JustAnotherHand: require('./src/assets/fonts/JustAnotherHand-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
       <BottomTabs />
--- myApp/src/navigation/BottomTabs.tsx
+++ myApp/src/navigation/BottomTabs.tsx
@@ -1,17 +1,112 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, StyleSheet } from 'react-native';
 
 import SettingScreen from '../screens/SettingScreen';
 import StackScreen from '../screens/StackScreen';
 import ScanScreen from '../screens/ScanScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Stack" component={StackScreen} />
-      <Tab.Screen name="Scan" component={ScanScreen} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: true,
+        tabBarShowLabel: false,
+
+        headerStyle: {
+          backgroundColor: '#B7D1E6',
+          height: 120,
+        },
+
+        headerTitleStyle: {
+          fontFamily: 'JustAnotherHand',
+          fontSize: 32,
+          color: '#111111',
+        },
+
+        headerTitleAlign: 'center',
+
+        tabBarStyle: {
+          backgroundColor: '#B7D1E6',
+          borderTopWidth: 0,
+          height: 70,
+          position: 'absolute',
+        },
+      }}
+    >
+      <Tab.Screen
+        name='Stack'
+        component={StackScreen}
+        options={{
+          title: 'My products',
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                styles.tabBarIconStyle,
+                {
+                  backgroundColor: focused ? '#B7D1E6' : 'transparent',
+                  transform: focused ? [{ translateY: -28 }] : [],
+                },
+              ]}
+            >
+              <Ionicons name='layers' size={36} color='#111' />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Scan'
+        component={ScanScreen}
+        options={{
+          title: 'Scan product',
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                styles.tabBarIconStyle,
+                {
+                  backgroundColor: focused ? '#B7D1E6' : 'transparent',
+                  transform: focused ? [{ translateY: -28 }] : [],
+                },
+              ]}
+            >
+              <Ionicons name='scan' size={36} color='#111' />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Setting'
+        component={SettingScreen}
+        options={{
+          title: 'My Profile',
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[
+                styles.tabBarIconStyle,
+                {
+                  backgroundColor: focused ? '#B7D1E6' : 'transparent',
+                  transform: focused ? [{ translateY: -28 }] : [],
+                },
+              ]}
+            >
+              <Ionicons name='settings' size={36} color='#111' />
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarIconStyle: {
+    top: 12,
+    width: 100,
+    height: 60,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
